e2eb3e3861fd8e93ad1defedaf7cb52e15836d00
André Siqueira <siqueira.ad@gmail.com>
2022-02-17 09:37:45 -0300

    Personalizando o serviço

--- estoquews/src/main/java/br/com/caelum/estoque/ws/EstoqueWs.java
+++ estoquews/src/main/java/br/com/caelum/estoque/ws/EstoqueWs.java
@@ -2,8 +2,14 @@ package br.com.caelum.estoque.ws;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 
@@ -12,7 +18,20 @@ public class EstoqueWs {
 
 	private ItemDao dao = new ItemDao();
 	
-	public List<Item> getItens(){
+//	@WebMethod(operationName = "todosOsItens")
+//	@WebResult(name = "itens")
+//	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros){
+//		System.out.println("Chamando todosItens()");
+//		List<Filtro> lista = filtros.getLista();
+//		List<Item> itensResultado = dao.todosItens(lista);
+//		return new ListaItens(itensResultado);
+//	}
+	
+	@WebMethod(operationName = "todosOsItens")
+	@WebResult(name = "item")
+	@ResponseWrapper(localName = "itens")
+	@RequestWrapper(localName = "listaItens")
+	public List<Item> getItens(@WebParam(name="filtros") Filtros filtros){
 		System.out.println("Chamando todosItens()");
 		return dao.todosItens();
 	}
